7d38f5b18a18ad0b27d4abdc0dc17d8f520c1e3e
Caner Türkaslan <canerturkaslan1998@gmail.com>
2019-06-17 10:21:11 +0300

    Models fixed for id fields

--- ogrenci/tests/bolum_test.py
+++ ogrenci/tests/bolum_test.py
@@ -5,21 +5,13 @@ from ogrenci.models.bolumler import Bolumler
 class BolumModelTestCase(TestCase):
 
 
-    def bolum_olustur(self,bolum_adi,bolum_id):
+    def bolum_olustur(self,bolum_adi):
         Bolumler.objects.create(
             bolum_adi=bolum_adi,
-            bolum_id=bolum_id
-        )
+         )
 
-
-    def test_validate_bolum_id(self):
-        bolum_adi="matematik"
-        bolum_id="2"
-        self.bolum_olustur(bolum_adi,bolum_id)
-        assert isinstance(bolum_id, int) , "id is not an integer: %r" % bolum_id
 
     def test_validate_bolum_adi(self):
             bolum_adi = 2
-            bolum_id = 1
-            self.bolum_olustur(bolum_adi, bolum_id)
+            self.bolum_olustur(bolum_adi)
             assert isinstance(bolum_adi, str), "bolum_adi is not a string : %r" % bolum_adi
